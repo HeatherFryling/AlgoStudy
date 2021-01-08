e1d7645bbf46a5116c0dc599e177b066abf19479
Heather Fryling <heatherfrylingg@gmail.com>
2021-01-07 16:39:02 -0800

    Changed delete to use size instead of a slice. Improved efficiency

--- SortingSearching/HeapSort/heap.py
+++ SortingSearching/HeapSort/heap.py
@@ -28,8 +28,8 @@ class MinHeap:
     # If the parent does not exist, return -1.
     # SIGNATURE
     # parent :: Integer => Integer
-    def parent(self, arr, index):
-        if (index == 0 or index > (len(arr) - 1)):
+    def parent(self, index):
+        if (index == 0 or index > self.size - 1):
             return -1
         return (index - 1) // 2
 
@@ -38,18 +38,18 @@ class MinHeap:
     # If the child does not exist, return -1.
     # SIGNATURE
     # left_child :: Integer => Integer
-    def left_child(self, arr, index):
+    def left_child(self, index):
         child_dex = 2 * (index + 1) - 1
-        return child_dex if child_dex < len(arr) else -1
+        return child_dex if child_dex < self.size else -1
 
     # PURPOSE
     # Get the index of the right child of the given index.
     # If the child does not exist, return -1.
     # SIGNATURE
     # right_child :: Integer => Integer
-    def right_child(self, arr, index):
+    def right_child(self, index):
         child_dex = 2 * (index + 1)
-        return child_dex if child_dex < len(arr) else -1
+        return child_dex if child_dex < self.size else -1
 
     # PURPOSE
     # Insert a new value in the correct position in the heap.
@@ -57,27 +57,28 @@ class MinHeap:
     # insert :: Integer => None
     # TIME AND SPACE COMPLEXITY:
     # Time: O(logn), Space: O(1)
-    def insert(self, val):
-        self.heap.append(val)
+    def insert_to_heap(self, val):
+        self.size += 1
+        self.heap.insert(self.size - 1, val)
         index = len(self.heap) - 1
-        while(self.heap[self.parent(self.heap, index)] > self.heap[index] and self.parent(self.heap, index) >= 0):
-            self.swap(self.heap, self.parent(self.heap, index), index)
-            index = self.parent(self.heap, index)
+        while(self.heap[self.parent(index)] > self.heap[index] and self.parent(index) >= 0):
+            self.swap(self.heap, self.parent(index), index)
+            index = self.parent(index)
         return
 
     # PURPOSE
     # Determine if a given position has a left child.
     # SIGNATURE
     # has_left_child :: Integer => Boolean
-    def has_left_child(self, arr, index):
-        return self.left_child(arr, index) < len(arr) and self.left_child(arr, index) != -1
+    def has_left_child(self, index):
+        return self.left_child(index) < self.size and self.left_child(index) != -1
 
     # PURPOSE
     # Determine if a given position has a right child.
     # SIGNATURE
     # has_right_child :: Integer => Boolean
-    def has_right_child(self, arr, index):
-        return self.left_child(arr, index) < len(arr) and self.right_child(arr, index) != -1
+    def has_right_child(self, index):
+        return self.left_child(index) < self.size and self.right_child(index) != -1
 
     # PURPOSE
     # Given the index of a value that violates the heap property,
@@ -87,12 +88,12 @@ class MinHeap:
     def heapify(self, arr, index):
         min = index
         curr_pos = index
-        while(self.has_left_child(arr, curr_pos) and self.has_right_child(arr, curr_pos)):
-            left_child = self.left_child(arr, curr_pos)
-            right_child = self.right_child(arr, curr_pos)
+        while(self.has_left_child(curr_pos) and self.has_right_child(curr_pos)):
+            left_child = self.left_child(curr_pos)
+            right_child = self.right_child(curr_pos)
             min = curr_pos
             if arr[curr_pos] > arr[left_child]:
-                min = self.left_child(arr, curr_pos)
+                min = self.left_child(curr_pos)
             if arr[min] > arr[right_child]:
                 min = right_child
             if min == curr_pos:
@@ -100,9 +101,9 @@ class MinHeap:
             else:
                 self.swap(arr, curr_pos, min)
                 curr_pos = min
-        if self.has_left_child(arr, curr_pos):
-            if arr[curr_pos] > arr[self.left_child(arr, curr_pos)]:
-                self.swap(arr, curr_pos, self.left_child(arr, curr_pos))
+        if self.has_left_child(curr_pos):
+            if arr[curr_pos] > arr[self.left_child(curr_pos)]:
+                self.swap(arr, curr_pos, self.left_child(curr_pos))
         return
 
     # PURPOSE
@@ -114,8 +115,8 @@ class MinHeap:
     # space = O(1)
     def delete(self):
         min = self.get_min()
-        self.swap(self.heap, 0, len(self.heap) - 1)
-        self.heap = self.heap[:len(self.heap) - 1]
+        self.swap(self.heap, 0, self.size - 1)
+        self.size -= 1
         self.heapify(self.heap, 0)
         return min
 
@@ -126,7 +127,9 @@ class MinHeap:
     # TIME AND SPACE COMPLEXITY
     # time = O(1) space = O(1)
     def get_min(self):
-        return self.heap[0]
+        if self.size > 0:
+            return self.heap[0]
+        return None
 
     # PURPOSE
     # Take in a list of numbers and turn it into a valid min-heap.
@@ -136,3 +139,7 @@ class MinHeap:
         for i in range((len(arr) - 1) // 2, -1, -1):
             self.heapify(arr, i)
         return arr
+
+test_list = list(range(20))
+test_heap = MinHeap(test_list)
+print(test_heap.left_child(9))
--- SortingSearching/HeapSort/test_heap.py
+++ SortingSearching/HeapSort/test_heap.py
@@ -3,71 +3,71 @@ from heap import *
 def test_parent():
     test_list = list(range(20))
     test_heap = MinHeap(test_list)
-    assert(-1 == test_heap.parent(test_heap.heap, 0))
-    assert(0 == test_heap.parent(test_heap.heap, 1))
-    assert(0 == test_heap.parent(test_heap.heap, 2))
-    assert(1 == test_heap.parent(test_heap.heap, 3))
-    assert(1 == test_heap.parent(test_heap.heap, 4))
-    assert(2 == test_heap.parent(test_heap.heap, 5))
-    assert(2 == test_heap.parent(test_heap.heap, 6))
-    assert(3 == test_heap.parent(test_heap.heap, 7))
-    assert(3 == test_heap.parent(test_heap.heap, 8))
-    assert(test_heap.parent(test_heap.heap, 9) == 4)
-    assert(test_heap.parent(test_heap.heap, 10) == 4)
-    assert(test_heap.parent(test_heap.heap, 11) == 5)
-    assert(test_heap.parent(test_heap.heap, 12) == 5)
-    assert(test_heap.parent(test_heap.heap, 13) == 6)
-    assert(test_heap.parent(test_heap.heap, 14) == 6)
-    assert(test_heap.parent(test_heap.heap, 15) == 7)
-    assert(test_heap.parent(test_heap.heap, 16) == 7)
-    assert(test_heap.parent(test_heap.heap, 17) == 8)
-    assert(test_heap.parent(test_heap.heap, 18) == 8)
-    assert(test_heap.parent(test_heap.heap, 19) == 9)
-    assert(test_heap.parent(test_heap.heap, 20) == -1)
+    assert(-1 == test_heap.parent(0))
+    assert(0 == test_heap.parent(1))
+    assert(0 == test_heap.parent(2))
+    assert(1 == test_heap.parent(3))
+    assert(1 == test_heap.parent(4))
+    assert(2 == test_heap.parent(5))
+    assert(2 == test_heap.parent(6))
+    assert(3 == test_heap.parent(7))
+    assert(3 == test_heap.parent(8))
+    assert(test_heap.parent(9) == 4)
+    assert(test_heap.parent(10) == 4)
+    assert(test_heap.parent(11) == 5)
+    assert(test_heap.parent(12) == 5)
+    assert(test_heap.parent(13) == 6)
+    assert(test_heap.parent(14) == 6)
+    assert(test_heap.parent(15) == 7)
+    assert(test_heap.parent(16) == 7)
+    assert(test_heap.parent(17) == 8)
+    assert(test_heap.parent(18) == 8)
+    assert(test_heap.parent(19) == 9)
+    assert(test_heap.parent(20) == -1)
 
 def test_left_child():
     test_list = list(range(20))
     test_heap = MinHeap(test_list)
-    assert(test_heap.left_child(test_heap.heap, 0) == 1)
-    assert(test_heap.left_child(test_heap.heap, 1) == 3)
-    assert(test_heap.left_child(test_heap.heap, 2) == 5)
-    assert(test_heap.left_child(test_heap.heap, 3) == 7)
-    assert(test_heap.left_child(test_heap.heap, 4) == 9)
-    assert(test_heap.left_child(test_heap.heap, 5) == 11)
-    assert(test_heap.left_child(test_heap.heap, 6) == 13)
-    assert(test_heap.left_child(test_heap.heap, 7) == 15)
-    assert(test_heap.left_child(test_heap.heap, 8) == 17)
-    assert(test_heap.left_child(test_heap.heap, 9) == 19)
+    assert(test_heap.left_child(0) == 1)
+    assert(test_heap.left_child(1) == 3)
+    assert(test_heap.left_child(2) == 5)
+    assert(test_heap.left_child(3) == 7)
+    assert(test_heap.left_child(4) == 9)
+    assert(test_heap.left_child(5) == 11)
+    assert(test_heap.left_child(6) == 13)
+    assert(test_heap.left_child(7) == 15)
+    assert(test_heap.left_child(8) == 17)
+    assert(test_heap.left_child(9) == 19)
 
 
 def test_right_child():
     test_list = list(range(20))
     test_heap = MinHeap(test_list)
-    assert(test_heap.right_child(test_heap.heap, 0) == 2)
-    assert(test_heap.right_child(test_heap.heap, 1) == 4)
-    assert(test_heap.right_child(test_heap.heap, 2) == 6)
-    assert(test_heap.right_child(test_heap.heap, 3) == 8)
-    assert(test_heap.right_child(test_heap.heap, 4) == 10)
-    assert(test_heap.right_child(test_heap.heap, 5) == 12)
-    assert(test_heap.right_child(test_heap.heap, 6) == 14)
-    assert(test_heap.right_child(test_heap.heap, 7) == 16)
-    assert(test_heap.right_child(test_heap.heap, 8) == 18)
-    assert(test_heap.right_child(test_heap.heap, 9) == -1)
+    assert(test_heap.right_child(0) == 2)
+    assert(test_heap.right_child(1) == 4)
+    assert(test_heap.right_child(2) == 6)
+    assert(test_heap.right_child(3) == 8)
+    assert(test_heap.right_child(4) == 10)
+    assert(test_heap.right_child(5) == 12)
+    assert(test_heap.right_child(6) == 14)
+    assert(test_heap.right_child(7) == 16)
+    assert(test_heap.right_child(8) == 18)
+    assert(test_heap.right_child(9) == -1)
 
 def test_insert():
     test_list = list(range(20))
     test_heap = MinHeap(test_list)
 
     # Adding a value on the end.
-    test_heap.insert(20)
+    test_heap.insert_to_heap(20)
     assert(list(range(21)) == test_heap.heap)
 
     # Adding a value at the root.
-    test_heap.insert(-1)
+    test_heap.insert_to_heap(-1)
     assert([-1, 0, 2, 3, 1, 5, 6, 7, 8, 9, 4, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 10] == test_heap.heap)
 
     # Adding a value in the middle
-    test_heap.insert(3)
+    test_heap.insert_to_heap(3)
     assert([-1, 0, 2, 3, 1, 5, 6, 7, 8, 9, 3, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 10, 4] == test_heap.heap)
 
 def test_delete():
@@ -77,25 +77,26 @@ def test_delete():
     assert(0 in test_heap.heap)
     min = test_heap.delete()
     assert(0 == min)
-    assert(0 not in test_heap.heap)
-    assert([1, 3, 2, 7, 4, 5, 6, 15, 8, 9, 10, 11, 12, 13, 14, 20, 16, 17, 18, 19] == test_heap.heap)
+    assert(20 == test_heap.size)
+    assert([1, 3, 2, 7, 4, 5, 6, 15, 8, 9, 10, 11, 12, 13, 14, 20, 16, 17, 18, 19, 0] == test_heap.heap)
 
     min = test_heap.delete()
     assert(1 == min)
-    assert(1 not in test_heap.heap)
-    assert([2, 3, 5, 7, 4, 11, 6, 15, 8, 9, 10, 19, 12, 13, 14, 20, 16, 17, 18] == test_heap.heap)
+    assert(19 == test_heap.size)
+    assert([2, 3, 5, 7, 4, 11, 6, 15, 8, 9, 10, 19, 12, 13, 14, 20, 16, 17, 18, 1, 0] == test_heap.heap)
 
     min = test_heap.delete()
     assert(2 == min)
-    assert(2 not in test_heap.heap)
-    assert([3, 4, 5, 7, 9, 11, 6, 15, 8, 18, 10, 19, 12, 13, 14, 20, 16, 17] == test_heap.heap)
+    assert(18 == test_heap.size)
+    assert([3, 4, 5, 7, 9, 11, 6, 15, 8, 18, 10, 19, 12, 13, 14, 20, 16, 17, 2, 1, 0] == test_heap.heap)
 
 def test_delete2():
     # Testing an initial list that is not sorted.
     test_list = [-4, 70, 12, 75, 83, 14, 16, 77, 78, 84, 900, 15, 16, 17, 22]
     test_heap = MinHeap(test_list)
     assert(-4 == test_heap.delete())
-    assert([12, 70, 14, 75, 83, 15, 16, 77, 78, 84, 900, 22, 16, 17] == test_heap.heap)
+    assert([12, 70, 14, 75, 83, 15, 16, 77, 78, 84, 900, 22, 16, 17, -4] == test_heap.heap)
+    assert(14 == test_heap.size)
 
 def test_delete3():
     # This one will test the branch in heapify that applies to a left child
@@ -103,7 +104,8 @@ def test_delete3():
     test_list = list(range(7))
     test_heap = MinHeap(test_list)
     assert(test_heap.delete() == 0)
-    assert([1, 3, 2, 6, 4, 5] == test_heap.heap)
+    assert([1, 3, 2, 6, 4, 5, 0] == test_heap.heap)
+    assert(6 == test_heap.size)
 
 def test_build_heap():
     test_list1 = list(range(11))
@@ -113,3 +115,12 @@ def test_build_heap():
     test_list2 = [99, 7, 12, 4, 8, 9, -1, 6, -900]
     test_heap2 = MinHeap(test_list2)
     assert([-900, 4, -1, 6, 8, 9, 12, 99, 7] == test_heap2.heap)
+
+def test_size():
+    test_list1 = list(range(11))
+    test_heap1 = MinHeap(test_list1)
+    assert(0 == test_heap1.get_min())
+    assert(11 == test_heap1.size)
+    test_heap1.delete()
+    assert(10 == test_heap1.size)
+    assert(1 == test_heap1.get_min())
