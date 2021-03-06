91b459f78628ffd6d8d8d28fb7c1af3f276d1df5
Heather Fryling <heatherfrylingg@gmail.com>
2021-03-05 20:41:16 -0800

    Completed bfs and bfs_all documentation.

--- GraphAlgos/BFS/basic_bfs.py
+++ GraphAlgos/BFS/basic_bfs.py
@@ -5,9 +5,16 @@
 from digraph import *
 from collections import deque
 
-def bfs(g, start):
-    visited = set({})
-    traversal = []
+# PURPOSE
+# Perform a breadth-first traversal of a directed graph in adjacency list
+# format from a given starting point and return the traversal as a list.
+# SIGNATURE
+# bfs :: Digraph, Integer, Set, List => List
+# TIME COMPLEXITY
+# O(m)
+# SPACE COMPLEXITY
+# O(n) -- for the traversal array and the visited array
+def bfs(g, start, visited, traversal):
     q = deque()
     q.append(start)
     visited.add(start)
@@ -20,11 +27,21 @@ def bfs(g, start):
                 visited.add(v)
     return traversal
 
-g = DiGraph()
-g.add_edge(1, 2)
-g.add_edge(1, 3)
-g.add_edge(2, 3)
-g.add_edge(2, 4)
-g.add_edge(4, 3)
-print(g)
-print(bfs(g, 1))
+# PURPOSE
+# Perform a breadth-first search of a given directed graph in adjacency list
+# format. Be sure to traverse all vertices by looping over all possible
+# starting points. Return a list containing the node labels in order of
+# traversal.
+# SIGNATURE
+# bfs_all :: Digraph => List
+# TIME COMPLEXITY
+# O(n + m) -- loop over very node and every edge
+# SPACE COMPLEXITY
+# O(n) -- for the visited and traversal arrays.
+def bfs_all(g):
+    traversal = []
+    visited = set({})
+    for start in g.graph.keys():
+        if start not in visited:
+            bfs(g, start, visited, traversal)
+    return traversal
--- GraphAlgos/BFS/test_basic_bfs.py
+++ GraphAlgos/BFS/test_basic_bfs.py
@@ -7,4 +7,14 @@ def test_bfs():
     g.add_edge(2, 3)
     g.add_edge(2, 4)
     g.add_edge(4, 3)
-    assert([1, 2, 3, 4] == bfs(g, 1))
+    assert([1, 2, 3, 4] == bfs(g, 1, set({}), []))
+
+def test_bfs_all():
+    g = DiGraph()
+    g.add_edge(1, 2)
+    g.add_edge(1, 3)
+    g.add_edge(2, 3)
+    g.add_edge(2, 4)
+    g.add_edge(4, 3)
+    g.add_vertex(5)
+    assert([1, 2, 3, 4, 5] == bfs_all(g))
