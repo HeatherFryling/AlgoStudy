cd916f561a95164499809e997157fc560f8e58db
Heather Fryling <heatherfrylingg@gmail.com>
2021-03-30 16:13:40 -0700

    Fixed an error in negative cycle detection.

--- GraphAlgos/ShortestPath/floyd_warshall/floyd_warshall.py
+++ GraphAlgos/ShortestPath/floyd_warshall/floyd_warshall.py
@@ -5,8 +5,6 @@
 # Given a graph in adjacency matrix format, return a matrix of the lengths
 # of all possible paths. Negative edge lengths are ok. Cycles are ok.
 # Negative cycles are not.
-# To detect negative cycles, we would need a 3d matrix and another check
-# in the algorithm.
 # SIGNATURE
 # floyd_warshall :: Graph (adjacency matrix) => List[List]
 # TIME COMPLEXITY
@@ -20,4 +18,8 @@ def floyd_warshall(g):
         for u in range(n):
             for v in range(n):
                 dist[u][v] = min(dist[u][v], dist[u][k] + dist[k][v])
+    # Detecting a negative cycle.
+    for u in range(n):
+        if dist[u][u] < 0:
+            return None
     return dist
