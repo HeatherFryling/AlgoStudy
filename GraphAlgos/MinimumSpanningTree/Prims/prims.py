16dca4e6b5164cc729d1f05ddaf572782a7446f2
Heather Fryling <heatherfrylingg@gmail.com>
2021-04-05 14:05:31 -0700

    Fixed Prims to use dist.

--- GraphAlgos/MinimumSpanningTree/Prims/prims.py
+++ GraphAlgos/MinimumSpanningTree/Prims/prims.py
@@ -18,27 +18,24 @@ def prims(g, weights):
     cut = [False for u in range(n)]
     parent = [None for u in range(n)]
     dist = [float('inf') for u in range(n)]
-    # Adding the first vertex to the cut.
-    u = 0
-    cut[0] = True
     parent[0] = None
     dist[0] = 0
-    cut_count = 1
+    cut_count = 0
     while(cut_count < n):
-        smallest_weight = float('inf')
-        smallest_v = None
+        smallest_dist = float('inf')
         smallest_u = None
+        # Inefficiently finding the smallest distance by iterating over the
+        # whole list of vertices.
         for u in range(n):
-            if cut[u]:
-                for v in g[u]:
-                    if cut[v] == False:
-                        if weights[u][v] < smallest_weight:
-                            smallest_weight = weights[u][v]
-                            smallest_u = u
-                            smallest_v = v
-        dist[smallest_v] = smallest_weight
-        parent[smallest_v] = smallest_u
-        cut[smallest_v] = True
+            if not cut[u]:
+                if dist[u] < smallest_dist:
+                    smallest_u = u
+                    smallest_dist = dist[u]
+        cut[smallest_u] = True
+        for v in g[smallest_u]:
+            if weights[smallest_u][v] < dist[v]:
+                dist[v] = weights[smallest_u][v]
+                parent[v] = smallest_u
         cut_count += 1
     return parent
 
@@ -137,3 +134,19 @@ def prims_custom_efficient(g, weights):
                     hd[y] = weights[(y, w)]
                     cheapest[y] = w
     return parent
+
+# g1 = [[1, 2, 3], [0, 3], [0, 3], [0, 1, 2]]
+# weights1 = [[0, 1, 2, 2],
+#             [1, 0, float('inf'), 4],
+#             [2, float('inf'), 0, 3],
+#             [2, 4, 3, 0]]
+# ans1 = [None, 0, 0, 0]
+# print(prims(g1, weights1))
+
+# # g2 = [[1, 2, 3], [0, 3], [0, 3], [0, 1, 2]]
+# # weights2 = [[0, 1, 4, 3],
+# #             [1, 0, float('inf'), 2],
+# #             [4, float('inf'), 0, 5],
+# #             [3, 2, 5, 0]]
+# # ans2 = [None, 0, 0, 1]
+# # assert(ans2 == prims(g2, weights2))
