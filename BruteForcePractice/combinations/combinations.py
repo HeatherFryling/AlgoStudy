aa5fcef9a1d30ebfc659e7ea106778192d4fdc9e
Heather Fryling <heatherfrylingg@gmail.com>
2021-09-22 20:20:07 -0700

    Started brute force powersets.

--- BruteForcePractice/combinations/combinations.py
+++ BruteForcePractice/combinations/combinations.py
@@ -1,47 +1,115 @@
 # Heather Fryling
-# 4/21/2021
-
-# The purpose of this file is to practice brute force algorithms that
-# cycle through all possible combinations of elements in an array.
-
-# Starting off with my Python implementation of Java code found at
-# https://www.baeldung.com/java-combinations-algorithm, just to get
-# a feel for how the process works.
+# 9/22/2021
 
 # PURPOSE
-# Generate a list of all possible combinations of values from 0 to n - 1 of
-# length r.
+# Count the number of combinations possible in a powerset.
 # SIGNATURE
-# generate :: Integer, Integer => List[List]
-# TIME COMPLEXITY
-# O(nCr)*r -- Time will be dominated by the leaves.
-# SPACE COMPLEXITY
-# O(n) calls on the stack.
-# O(nCr) space for the combinations list.
-def generate(n, r):
-    combinations = []
-    data = [0 for x in range(r)]
-    print(data)
-    helper(combinations, data, 0, n-1, 0)
-    return combinations
+# countPowerset :: List[Integer] => Integer
+# TIME AND SPACE COMPLEXITY
+# O(2^n) time | O(n) space
+def countPowerset(nums):
+    return countP(nums, 0)
 
 # PURPOSE
-# Recursively generate combinations based upon the cases:
-#    * Base case: Once r elements are chosen, stop.
-#    * Recursive cases:
-#      * Take the element at start.
-#      * Do not take the element at start.
-def helper(combinations, data, start, end, num_chosen):
-    if (num_chosen == len(data)):
-        combination = data.copy()
-        combinations.append(combination)
-    elif (start <= end):
-        # This is ok to do for both cases, because it will be overwritten if
-        # num_chosen is not incremented.
-        data[num_chosen] = start
-        helper(combinations, data, start + 1, end, num_chosen + 1)
-        helper(combinations, data, start + 1, end, num_chosen)
+# Helper function to recursively count the number of combinations possible in a powerset.
+# SIGNATURE
+# countPowerset :: List[Integer] , Integer=> Integer
+def countP(nums, i):
+    # Base case: The powerset is as long as the whole array.
+    if i == len(nums):
+        return 1
 
-n = 4
-r = 2
-print(generate(n, r))
+    # For the next element, we can include it or excluse it.
+    # Branching factor is 2.
+    include = countP(nums, i + 1)
+    exclude = countP(nums, i + 1)
+
+    return include + exclude
+
+# PURPOSE
+# Return the powerset of a list of items.
+# SIGNATURE
+# powerSet :: List => List[List]
+# TIME AND SPACE COMPLEXITY
+# O(n*2^n) time | O(n*2^n) space
+def powerSet(nums, i = None):
+    # If no index is provided, start with the end of the list.
+    if i is None:
+        i = len(nums) - 1
+    # If the whole list is processed, return the empty set.
+    if i < 0:
+        return [[]]
+    subsets = powerSet(nums, i - 1)
+    for idx in range(len(subsets)):
+        currentSubset = subsets[idx]
+        subsets.append(currentSubset + [nums[i]])
+    return subsets
+
+print(powerSet([1, 2, 3]))
+
+# PURPOSE
+# Return the powerset of a list of items.
+# SIGNATURE
+# powerSet :: List => List[List]
+# TIME AND SPACE COMPLEXITY
+# O(n*2^n) time | O(n*2^n) space
+def powerSet2(nums):
+    subsets = [[]]
+    for num in nums:
+        for i in range(len(subsets)):
+            currentSubset = subsets[i]
+            subsets.append(currentSubset + [num])
+    return subsets
+
+print(powerSet2([1, 2, 3]))
+
+# PURPOSE
+# Return the powerset of a list of items.
+# SIGNATURE
+# powerSet :: List => List[List]
+# TIME AND SPACE COMPLEXITY
+# O(n*2^n) time | O(n*2^n) space
+def powerSet3(nums):
+    results = []
+    powerSet3Helper(nums, 0, results, [])
+    return results
+
+def powerSet3Helper(nums, idx, results, path):
+    # All of them will hit the length of nums and get appended to the results.
+    if idx == len(nums):
+        results.append(path)
+        return
+    
+    # Make a copy of the previous subset.
+    pathWithCurrent = [p for p in path]
+    # Add the current item to the previous subset.
+    pathWithCurrent.append(nums[idx])
+
+    # Recursively call without using the current number.
+    powerSet3Helper(nums, idx + 1, results, path)
+    # Recursively call with using the current number.
+    powerSet3Helper(nums, idx + 1, results, pathWithCurrent)
+
+print(powerSet3([1,2,3]))
+
+# PURPOSE
+# Return the powerset of a list of items.
+# SIGNATURE
+# powerSet :: List => List[List]
+# TIME AND SPACE COMPLEXITY
+# O(n*2^n) time | O(n*2^n) space
+def powerSet4(nums, idx = 0):
+    # If the whole list is processed, return the empty set.
+    if idx == len(nums):
+        return [[]]
+    subsets = []
+    for subset in powerSet4(nums, idx + 1):
+        # Add a copy without the new number.
+        subsets.append([x for x in subset])
+        # Add a copy with the new number.
+        subset.append(nums[idx])
+        subsets.append([x for x in subset])
+
+    return subsets
+
+print(powerSet4([1, 2, 3]))
